2806c9ab5a2d1371f695a210763138df6e5161ec
chszchen <rafoul1013@gmail.com>
2025-05-14 17:16:13 +0800

    chore: trouble shooting action

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // Get the repository name from the environment variable (if set in GitHub Actions)
 const repoName = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
 
+console.log(`The repository name is ${repoName}`);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 9010
   },
-  base: repoName,
+  base: '/sic_site/',
   build: {
     outDir: 'dist',
     emptyOutDir: true
