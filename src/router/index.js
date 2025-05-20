8ed8bcb40c587ae91c7503476645a74b54101bc4
chszchen <rafoul1013@gmail.com>
2025-05-20 09:17:08 +0800

    chore: update deploy.yml

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import Register from '../components/Register.vue'
 
 // Get the base path from the environment variable (if set in GitHub Actions)
 const base = import.meta.env.VITE_GITHUB_REPOSITORY ? `/${import.meta.env.VITE_GITHUB_REPOSITORY.split('/')[1]}/` : '/';
-
+console.log(base);
 const router = createRouter({
   history: createWebHistory(base),
   routes: [
