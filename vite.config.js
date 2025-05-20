9ed607ca2764b5dd41c70c5841546eafb08c1148
chszchen <rafoul1013@gmail.com>
2025-05-20 09:51:22 +0800

    chore: update base

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // Get the repository name from the environment variable (if set in GitHub Actions)
-const base = process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/';
-
+const base = '/sic_site/';
 
 // https://vite.dev/config/
 export default defineConfig({
